375dff292854f52b065032b2df31fb86b9a92393
Buzorcodes <141915811+Buzorcodes@users.noreply.github.com>
2023-09-20 18:00:16 +0100

    Update MetaBank.sol

--- MetaBank.sol
+++ MetaBank.sol
@@ -1,33 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 
-contract MetaBank {
-    address public  owner;
-    uint public bankBalance;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Metacrafters is ERC20, Ownable {
+    constructor() ERC20("Metacrafters Token", "MTC") {}
+
+    function mintToken(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burnToken(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+ 
    
-    mapping(address => uint256) public balanceOf;
-
-    constructor() {
-        owner = msg.sender;
-      }
-
-    function mintToken( uint256 _value) public {
-        require(msg.sender == owner, "Only owner can call this function");
-        balanceOf[msg.sender] += _value;
-        bankBalance += _value;
-    }
-
-    function transferToken(address _receiver, uint256 _value) public {
-        require(balanceOf[msg.sender] >= _value, "low balance");
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_receiver] += _value;
-    }
-
-    function burnToken(uint256 _value) public {
-        require(balanceOf[msg.sender] >= _value, "low balance");
-        balanceOf[msg.sender] -= _value;
-        bankBalance -= _value;
-    }
-
-
 }
